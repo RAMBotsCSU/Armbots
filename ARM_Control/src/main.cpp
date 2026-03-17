327e557fc2187537ecfbe2eaf743e2cfba97010b
gabef116 <gabe116@colostate.edu>
2026-03-16 23:44:24 -0600

    Added GPIO pins

--- ARM_Control/src/main.cpp
+++ ARM_Control/src/main.cpp
@@ -1,4 +1,12 @@
 #include <Arduino.h>
+// PWM Pins
+int step_one = 1;
+int dir_1 = 2;
+int enable_L = 4;
+int dir_2 = 5;
+int ms1_M2 = 9;
+int ms2_M2 = 10;
+
 
 // put function declarations here:
 int myFunction(int, int);
